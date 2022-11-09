cae93c3ccfada4399a3775bea998d0615e0ae1a4
David-Martinez02 <dmart0101@jaguar.tamu.edu>
2022-11-08 20:36:14 -0600

    test

--- app/src/main/java/com/example/allinone/fragment_coin_flip.kt
+++ app/src/main/java/com/example/allinone/fragment_coin_flip.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class fragment_coin_flip : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coin_flip, container, false)
+        val view =  inflater.inflate(R.layout.fragment_coin_flip, container, false)
+        val button_flip = view.findViewById<Button>(R.id.button_flip)
+        button_flip.setOnClickListener{
+            Toast.makeText(context, "Test", Toast.LENGTH_LONG).show()
+        }
+        return view
     }
 
     companion object {
